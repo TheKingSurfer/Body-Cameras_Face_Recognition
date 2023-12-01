2befd4a2a27f022fc8c7a845153b9e050d370910
TheKingSurfer <eitanalbo06@gmail.com>
2023-12-01 23:24:54 +0200

    Added new file and the algorithm done with open cv

--- Face_logics/FaceRecognitionOpencv.py
+++ Face_logics/FaceRecognitionOpencv.py
@@ -2,6 +2,9 @@ import cv2
 from simple_facerec import SimpleFacerec
 
 
+
+detected_faces = []
+
 #Encode faces from folders
 sfr = SimpleFacerec()
 sfr.load_encoding_images("Images/BillGates/")
@@ -10,23 +13,30 @@ sfr.load_encoding_images("Images/BillGates/")
 cap = cv2.VideoCapture(0)
 
 
+# Load Camera
+
+
+
 while True:
     ret, frame = cap.read()
-    cv2.imshow("Frame", frame)
 
-    #detect faces
-    face_locations, face_names =sfr.detect_known_faces(frame)
+    # Detect Faces
+    face_locations, face_names = sfr.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
-        #print the quardinates of the face shown in the camera
-        print(face_loc)
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
+        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+        if name != "Unknown" and name != "images":
+            detected_faces.append(name)
+    cv2.imshow("Frame", frame)
 
 
 
     key = cv2.waitKey(1)
     if key == 27:
         break
+print(detected_faces)
 cap.release()
 cv2.destroyAllWindows()
-
-
 
